622aacc875891fba946058f93cbbe8c2651d142d
Scott Finlay <scott@magex.ca>
2015-03-27 09:02:13 -0400

    Personal vimrc script

---
+++ build/maven/exec-program/src/main/java/ca/magex/exec/MainClass.java
@@ -0,0 +1,10 @@
+package ca.magex.exec;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("My args are: " + args);
+		System.out.println("Sysprop: " + System.getProperty("myproperty"));
+	}
+	
+}
