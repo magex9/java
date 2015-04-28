1913bf3f1379cb57dc7f97af9670c92ef2b737b9
magex9 <magex@abiosis.local>
2015-04-28 12:53:53 -0400

    Updated demos

--- docker/tomcat-demo/run.sh
+++ docker/tomcat-demo/run.sh
@@ -7,4 +7,4 @@ docker build -t magex9/tomcat-demo .
 docker run -d -p 9001:8080 magex9/tomcat-demo
 
 # Open the browser to the start page
-open http://$(boot2docker ip 2>/dev/null):9001/examples/
+open http://$(boot2docker ip 2>/dev/null):9001/tomcat/
