326c76dab8fb27585e71638e1fb0b9a81c82cec3
magex9 <magex@abiosis.local>
2015-05-02 12:53:19 -0400

    removed duplicate scripts

--- docker/nginx/shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build a new versio of the image
-docker build -t magex9/nginx .
-
-# Star the server on the same port
-docker run -i -t -p 80:80 magex9/nginx /bin/bash
-
---
+++ docker/proxy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Stop and remove old containers
+docker stop nginx; docker rm nginx;
+docker stop proxy-nexus; docker rm proxy-nexus;
+docker stop proxy-jquery; docker rm proxy-jquery;
+docker stop proxy-jboss; docker rm proxy-jboss;
+
+# Build a new versio of the image
+docker build -t magex9/nginx .
+
+docker run --name proxy-jboss -d -p 20001:8080 magex9/jboss
+docker run --name proxy-jquery -d -p 20003:8080 magex9/tomcat-demo
+docker run --name proxy-nexus -d -p 20004:8081 magex9/nexus
+
+# Star the server on the same port
+docker run --name nginx -d -p 80:80 magex9/nginx
+
