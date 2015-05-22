c06f31adb39e15bb40c187fa39901cb504c7ab87
magex9 <magex@magex.ca>
2015-05-21 20:02:41 -0400

    Updated the way screenshots are taken.

--- web/selenium-screenshot/src/main/java/ca/magex/selenium/Website.java
+++ web/selenium-screenshot/src/main/java/ca/magex/selenium/Website.java
@@ -1,11 +1,10 @@
 package ca.magex.selenium;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -17,71 +16,88 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Website {
 
+	private static final Logger logger = Logger.getLogger(Website.class.getName());
+	
 	public static void main(String[] args) throws Exception {
-		new Website().run("http://csszengarden.com/");
+		new Website().run("http://magex9.github.io/workspace/");
 	}
 
 	private String base;
 	
 	private WebDriver driver;
 	
-	private Set<String> processed;
+	private String datadir;
 	
-	private String dir;
+	private Set<String> processed;
 	
 	public void run(String url) throws Exception {
 		this.base = url;
 		if (!base.endsWith("/"))
 			base = base.substring(0, base.lastIndexOf("/") + 1);
-		this.driver = new FirefoxDriver();
 		this.processed = new HashSet<String>();
-		this.dir = Calendar.getInstance().getTimeInMillis() + "";
-		screenshot(url);
-		followLinks();
+		this.driver = new FirefoxDriver();
+		this.datadir = "target/data/" + base.replaceAll("[^a-z]", "_").replaceAll("__*", "_");
+		process(url);
 		driver.quit();
-	}	
-		
-	private void followLinks() throws Exception {
-		System.out.println("Following links");
-		List<String> links = new ArrayList<String>();
-		for (WebElement element : driver.findElements(By.tagName("a"))) {
-			String href = element.getAttribute("href");
-			if (href.indexOf("#") > 0)
-				href = href.substring(0, href.indexOf("#"));
-			if (href.indexOf("?") > 0)
-				href = href.substring(0, href.indexOf("?"));
-			links.add(href);
-			System.out.println("Found: " + href);
-		}
-
-		for (String key : links) {
-			if (screenshot(key)) {
-				followLinks();
-			}
-		}
-		
 	}
 	
-	public boolean screenshot(String href) throws Exception {
-		if (!href.startsWith(base) || href.endsWith(".pdf")
-				|| href.endsWith(".css") || href.endsWith(".js"))
-			return false;
+	public void process(String url) throws Exception {
+		if (!url.startsWith(base) || !(url.endsWith(".asp")
+				|| url.endsWith(".html")))
+			return;
+
+		if (processed.contains(url))
+			return;
 		
-		if (processed.contains(href)) {
-			System.out.println("Skipping: " + href);
-			return false;
+		String filename = url.replaceFirst(base, "").replaceAll("/", "_");
+		File screenshot = new File(datadir + "/" + filename + ".png");
+		File source = new File(datadir + "/" + filename + ".html");
+		File links = new File(datadir + "/" + filename + ".links");
+		File complete = new File(datadir + "/" + filename + ".done");
+		
+		if (complete.exists())
+			return;
+
+		logger.info("Processing: " + url);
+		
+		if (!screenshot.exists() || !links.exists() || !source.exists()) {
+			driver.get(url);
+		}
+
+		if (!screenshot.exists()) {
+			logger.info("Writing screenshot: " + screenshot.getAbsolutePath());
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, screenshot);
 		}
 		
-		driver.get(href);
-		String name = driver.getTitle();
-		System.out.println("Goto: " + href + " for " + name);
-		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		String filename = href.replaceFirst(base, "").replaceAll("/", "_") + " - " + name;
-		File output = new File("target/" + dir + "/" + filename + ".png");
-		FileUtils.copyFile(scrFile, output);
+		if (!links.exists()) {
+			StringBuilder sb = new StringBuilder();
+			for (WebElement element : driver.findElements(By.tagName("a"))) {
+				String href = element.getAttribute("href");
+				logger.info("Found link: " + href);
+				if (href.indexOf("#") > 0)
+					href = href.substring(0, href.indexOf("#"));
+				if (href.indexOf("?") > 0)
+					href = href.substring(0, href.indexOf("?"));
+				sb.append("\n");
+				sb.append(href);
+			}
+			logger.info("Writing links: " + links.getAbsolutePath());
+			FileUtils.writeStringToFile(links, sb.substring(1));
+		}
 		
-		processed.add(href);
-		return true;
+		if (!source.exists()) {
+			logger.info("Writing source: " + source.getAbsolutePath());
+			FileUtils.writeStringToFile(source, driver.getPageSource());
+		}
+		
+		processed.add(url);
+		
+		for (String link :  FileUtils.readFileToString(links).split("\n")) {
+			process(link);
+		}
+		
+		FileUtils.writeStringToFile(complete, new Date().toString());
 	}
 	
 }
