a4caf9d96edb9c053cc0dfe106aef16ead3d589d
magex9 <magex@magex.ca>
2015-11-12 10:25:39 -0500

    Ideas

---
+++ jena/rdf-generator/src/main/java/ca/magex/jena/rdf/generator/PicturesGenerator.java
@@ -0,0 +1,14 @@
+package ca.magex.jena.rdf.generator;
+
+public class PicturesGenerator {
+
+	
+//		topic://resource/picture/md5/0294029409	prop:tag		'2015-12 - Christmas'
+//		topic://resource/picture/md5/0294029409	prop:filename	'DSC_04949.jpg'
+//		topic://resource/picture/md5/0294029409	prop:path		'/Pictures/2015-12 - Christmas/'
+//		topic://resource/picture/md5/0294029409	prop:thumbnail	'400920920490924094=='
+//		topic://resource/picture/md5/0294029409	prop:mimetype	topic://system/mimetype/jpg
+
+
+	
+}
---
+++ jena/rdf-generator/src/main/java/ca/magex/jena/rdf/generator/ProjectGenerator.java
@@ -0,0 +1,38 @@
+package ca.magex.jena.rdf.generator;
+
+public class ProjectGenerator {
+
+//	Sushi Menu
+//
+//	ref://computer/software/sushi																				2.0			2.1
+//		prop:common.topic.name							'Sushi Menu'			;
+//		prop:software.application.business_id			ref:software.business.ops	;
+//		prop:software.application.product_id			'sushi'						;
+//
+//		prop:software.security.planned_caching			ref:software.caching.none	;
+//		prop:software.security.character				ref:software.text.utf8		;
+//		prop:software.security.character				ref:software.text.xml		;
+//		prop:software.security.client_server			ref:software.authentication.user_pass	;
+//		prop:software.security.server_data				ref:software.authentication.static_user	;
+//		
+//		prop:software.dependency.server					ref:software.server.rhel64			;					6.3			6.4
+//		prop:software.dependency.container				ref:software.container.jboss52		;					5.1.0		5.2.0
+//		prop:software.dependency.library				ref:software.service.lookup			;					2.1.3		4.3.2
+//		prop:software.dependency.library				ref:software.service.parameter		;					1.2.7		1.3.9
+//		prop:software.dependency.library				ref:software.service.authentication	;					3.0			3.2.3
+//		prop:software.dependency.application			ref:software.application.outlook	;					
+//		prop:software.dependency.service				ref:software.application.oracle		;
+//		
+//		prop:software.deployment.environment			ref:devops.deployment.ut1			;
+//		prop:software.deployment.environment			ref:devops.deployment.ut2			;
+//		
+//		prop:software.validation.storage_validation		ref:software.validation.parameterizd_query	;
+//		prop:software.validation.user_input_validation	ref:software.validation.athena3		;
+//		prop:software.validation.complex_form			ref:software.validation.none		;
+//		
+//		prop:software.storage.data_server				ref:software.service.oracle10		;
+//		prop:software.storage.structures				ref:software.storage.custom3nf		;
+//
+
+	
+}
